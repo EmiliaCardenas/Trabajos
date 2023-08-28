ceac44477d0e1c52f180c77e328ddbaaee8e45c7
EmiliaCardenas <142023291+EmiliaCardenas@users.noreply.github.com>
2023-08-28 16:18:50 -0600

    proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -1,81 +1,145 @@
 """
-Se enseña como habrá diferentes sumas en fuciones (que se haran luego)
-Y dependiendo la suma habran diferentes resultado
+Se inicia pidiendo el nombre de usuario por cortesia
+"""
+print("Escribre tu nombre, por favor: ")
+nombre = str(input())
+print("Bienvenidx a este quiz de anime", nombre)
+
+"""
+Va a ser una pantalla extra, la cual se abrirá al abrir el codigo
+Las preguntas algunas seran radiobutton y otras checkbutton
+Se enseña como habrá diferentes sumas en fuciones 
+Y dependiendo la suma habran diferentes resultados
 """
 
 """
 Pregunta de si es preferible un pelicula o una serie o ambas
-Teniendo solamente tres opciones
+Teniendo solamente tres opciones (radiobutton)
 """
-pelicula = 1
-serie = 2
-ambas = pelicula + serie
+print("¿Que prefieres: pelicula, serie, o ambas?")
+
+if radiobutton1 == pelicula:
+    res = 1
+elif radiobutton1 == serie:
+    res = 2
+elif radiobutton1 == ambas:
+    res = 3
+else:
+    res = 0
 
 """
 Pregunta de que cual es la cantidad de episodios preferida
 si es escogida la opcion de serie
-Teniendo cuatro opciones para elgir
+Teniendo cuatro opciones para elgir (radiobutton)
 """
-poco ("1-18") = 1
-medio ("19-29") = 2
-mucho ("30<") = 3
-nada ("sin importar la cantidad") = 4
+print("¿Que cantidad de capitulos prefieres?")
+poco "1-18"
+medio "19-29"
+mucho "30<"
+nada "sin importar la cantidad"
+if radiobutton2 == poco:
+    res = 1
+elif radiobutton2 == medio:
+    res = 2
+elif radiobutton2 == mucho:
+    res = 3
+elif radiobutton2 == nada:
+    res = 4
+else:
+    res = 0
 
 """
 Pregunta de que generos son los elegidos y preferidos por el usuario
-Teniendo varias opciones para escoger, siendo un maxico de 2 las que
-el usuario podrá elegir
+Teniendo varias opciones para escoger, siendo un maximo de 2 las que
+el usuario podrá elegir (checkbutton)
 """
+print("¿Que genero prefieres (escoge 2 como maximo)")
 
-romance = 1
-comedia = 2
-drama = 3
-accion = 4
-ciencia_ficcion = 5
-aventura = 6
-misterio = 7
-terror = 8
+if checkbutton3 == romance:
+    res = 1
+elif checkbutton3 == comedia:
+    res = 2
+elif checkbutton3 == drama:
+    res = 3
+elif checkbutton3 == accion:
+    res = 4
+elif checkbutton3 == ciencia_ficcion:
+    res = 5
+elif checkbutton3 == aventura:
+    res = 6
+elif checkbutton3 == misterio:
+    res = 7
+elif checkbutton3 == terrror:
+    res = 8
+else:
+    res = 0
 
 """
 Pregunta de que generos especificos de anime son los elegidos y
 preferidos por el usuario
 Teniendo varias opciones para escoger, siendo un maxico de 3 las que
-el usuario podrá elegir
+el usuario podrá elegir (checkbutton)
 A futuro va a haber una mini explicacion de cada genero
 """
-
-shounen = 1
-isekai = 2
-superpoderes = 3
-mecha = 4
-bl = 5
-gl = 6
-shoujo = 7
-escolar = 8
-sol ("Slice of life") = 9
-josei = 10
-seinen = 11
-gore = 12
+print("¿Que genero de anime prefieres (escoge 3 como maximo)")
+if checkbutton4 == shounen:
+    res = 1
+elif checkbutton4 == isekai:
+    res = 2
+elif checkbutton4 == supepoderes:
+    res = 3
+elif checkbutton4 == mecha:
+    res = 4
+elif checkbutton4 == bl:
+    res = 5
+elif checkbutton4 == gl:
+    res = 6
+elif checkbutton4 == shoujo:
+    res = 7
+elif checkbutton4 == escolar:
+    res = 8
+elif checkbutton4 == sol:
+    res = 9
+elif checkbutton4 == josei:
+    res = 10
+elif checkbutton4 == seinen:
+    res = 11
+elif checkbutton4 == gore:
+    res = 12
+else:
+    res = 0
 
 """
-Pregunta de si les gustan los deportes o no
+Pregunta de si les gustan los deportes o no (radiobutton)
 al elegir la opcion de si, se despeglaran las preguntas (solo si es serie)
 Teniendo varias opciones para escoger, siendo un maxico de 2 las que
-el usuario podrá elegir
+el usuario podrá elegir (checkbutton)
 """
-deporte
-si = 1
-no = 0
+print("¿Te gustan los deportes? Si sí, escoge cuales (maximo 2)")
 
-fut = 1
-base = 2
-basq = 3
-nat = 4
-golf = 5
-tenis = 6
-gymna = 7
-patin = 8
-skate = 9
+if radiobutton5 == si:
+    if checkbutton5 == football:
+        res = 1
+    elif checkbutton5 == baseball:
+        res = 2
+    elif checkbutton5 == basquetball:
+        res = 3
+    elif checkbutton5 == natacion:
+        res = 4
+    elif checkbutton5 == golf:
+        res = 5
+    elif checkbutton5 == tenis:
+        res = 6
+    elif checkbutton5 == gymnasia:
+        res = 7
+    elif checkbutton5 == patinaje:
+        res = 8
+    elif checkbutton5 == skate:
+        res = 9
+elif radiobutton5 == no:
+    res = 1
+else:
+    res = 0
 
 """
 Una vez esten hechas las funciones se podran compilar las opciones
@@ -84,12 +148,15 @@ asi sabien que programas son mas compatibles con las opciones
 siendo que se van a dividir en peliculas y series
 se creará un rango en las opciones para que sea mas amplia la gama 
 Un ejemplo se demuestra abajo
-"""
 
-"""
 2 peliculas
 """
 Viaje_de_chihiro = (pelicula) + ("Genero" aventura + drama) + \
                    ("genero anime" isekai) + (deporte = no)
+"""
+Viaje_de_chihiro daria pelicula = 1 genero = 9, genero anime = 2,
+deporte 0. Cada una de las sumas en un caso a parte que al final ayuda a ver
+si es elegida o no por el usuario
+"""
 I_wanna_eat_you_pancreas = (pelicula) + ("Genero" comedia + drama) + \
                            ("genero anime" shoujo + sol + josei) + (deporte = no)
