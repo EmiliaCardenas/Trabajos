a3ea51572eae65055ad6d5b7d5ee32e39b096395
EmiliaCardenas <142023291+EmiliaCardenas@users.noreply.github.com>
2023-09-03 09:39:32 -0600

    proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -1,74 +1,58 @@
 """
-Se inicia pidiendo el nombre de usuario por cortesia
-Va a ser una pantalla extra, la cual se abrirá al abrir el codigo
+Va a ser una pantalla extra (tkinter), la cual se abrirá al abrir el codigo
 Las preguntas algunas seran radiobutton y otras checkbutton
 Se enseña como habrá diferentes sumas en fuciones 
 Y dependiendo la suma habran diferentes resultados
 """
-
+import tkinter
+quiz_anime = tkinter.Tk()
+quiz_anime.configure(bg="white")
+quiz_anime.geometry("700x700")
+quiz_anime.title("Quiz Anime")
 """
 Pregunta de si es preferible una pelicula o una serie o ambas
 Teniendo solamente tres opciones (radiobutton)
 """
-print("¿Que formato prefieres: pelicula, serie, o ambas?")
-def formato (radiobutton1):
-    if radiobutton1 == pelicula:
-        res = 1
-    elif radiobutton1 == serie:
-        res = 2
-    elif radiobutton1 == ambas:
-        res = 3
-    else:
-        res = 0
+text2 = tkinter.Label(quiz_anime,
+                      text="¿Que formato prefieres: pelicula, serie, o ambas?")
+text2.pack(anchor="w")
+eleccion1 = ["pelicula", "serie", "ambas"]
+formato = tkinter.StringVar()
+for index in range(len(eleccion1)):
+    radiobutton1 = tkinter.Radiobutton(quiz_anime, text= eleccion1[index],
+                                       variable = formato,indicatoron=0,
+                                       value=index, width=10).pack(anchor="w")
 
 """
 Pregunta de que cual es la cantidad de episodios preferida
 si es escogida la opcion de serie
 Teniendo cuatro opciones para elgir (radiobutton)
 """
-print("¿Que cantidad de capitulos prefieres?")
-poco "1-18"
-medio "19-29"
-mucho "30<"
-nada "sin importar la cantidad"
-def capitulo (radiobutton2):
-    if radiobutton2 == poco:
-        res = 1
-    elif radiobutton2 == medio:
-        res = 2
-    elif radiobutton2 == mucho:
-        res = 3
-    elif radiobutton2 == nada:
-        res = 4
-    else:
-        res = 0
-
+text3 = tkinter.Label(quiz_anime,
+                      text="¿Que cantidad de capitulos prefieres?")
+text3.pack(anchor="w")
+eleccion2 = ["1 a 18","19 a 29","más de 30","sin importar la cantidad", "elegí pelicula"]
+cantidad = tkinter.StringVar()
+for index in range(len(eleccion2)):
+    radiobutton2 = tkinter.Radiobutton(quiz_anime, text= eleccion2[index],
+                                       variable = cantidad,indicatoron=0,
+                                       value=index, width=20).pack(anchor="w")
 """
 Pregunta de que generos son los elegidos y preferidos por el usuario
 Teniendo varias opciones para escoger, siendo un maximo de 2 las que
 el usuario podrá elegir (checkbutton)
 """
-print("¿Que genero prefieres (escoge 2 como maximo)")
-def genero (checkbutton3):
-    if checkbutton3 == romance:
-        res = 1
-    elif checkbutton3 == comedia:
-        res = 2
-    elif checkbutton3 == drama:
-        res = 3
-    elif checkbutton3 == accion:
-        res = 4
-    elif checkbutton3 == ciencia_ficcion:
-        res = 5
-    elif checkbutton3 == aventura:
-        res = 6
-    elif checkbutton3 == misterio:
-        res = 7
-    elif checkbutton3 == terrror:
-        res = 8
-    else:
-        res = 0
-
+text4 = tkinter.Label(quiz_anime,
+                      text="¿Que genero prefieres (escoge 2 como maximo)")
+text4.pack(anchor="w")
+eleccion3 = ["romance","comedia","drama","accion",
+             "ciencia ficcion", "aventura", "misterio", "terror"]
+genero = []
+for index in range(len(eleccion3)):
+    genero = tkinter.StringVar(value = 0)
+    checkbutton3 = tkinter.Checkbutton(quiz_anime, text= eleccion3[index],
+                                       variable = genero, indicatoron=0,
+                                       width=20).pack(anchor="w")
 """
 Pregunta de que generos especificos de anime son los elegidos y
 preferidos por el usuario
@@ -76,67 +60,74 @@ Teniendo varias opciones para escoger, siendo un maxico de 3 las que
 el usuario podrá elegir (checkbutton)
 A futuro va a haber una mini explicacion de cada genero
 """
-print("¿Que genero de anime prefieres (escoge 3 como maximo)")
-def generoanime (checkbutton4):
-    if checkbutton4 == shounen:
-        res = 1
-    elif checkbutton4 == isekai:
-        res = 2
-    elif checkbutton4 == supepoderes:
-        res = 3
-    elif checkbutton4 == mecha:
-        res = 4
-    elif checkbutton4 == bl:
-        res = 5
-    elif checkbutton4 == gl:
-        res = 6
-    elif checkbutton4 == shoujo:
-        res = 7
-    elif checkbutton4 == escolar:
-        res = 8
-    elif checkbutton4 == sol:
-        res = 9
-    elif checkbutton4 == josei:
-        res = 10
-    elif checkbutton4 == seinen:
-        res = 11
-    elif checkbutton4 == gore:
-        res = 12
-    else:
-        res = 0
+text5 = tkinter.Label(quiz_anime,
+                      text="¿Que genero de anime prefieres (escoge 3 como maximo)")
+text5.place(x=400, y=180)
 
+genero_anime = tkinter.StringVar()
+genero_anime2 = tkinter.StringVar()
+genero_anime3 = tkinter.StringVar()
+genero_anime4 = tkinter.StringVar()
+genero_anime5 = tkinter.StringVar()
+genero_anime6 = tkinter.StringVar()
+genero_anime7 = tkinter.StringVar()
+genero_anime8 = tkinter.StringVar()
+genero_anime9 = tkinter.StringVar()
+genero_anime10 = tkinter.StringVar()
+genero_anime11 = tkinter.StringVar()
+genero_anime12 = tkinter.StringVar()
+
+c41 = tkinter.Checkbutton(quiz_anime, text = "shounen", indicatoron=0,
+                          variable = genero_anime, onvalue = "shounen",
+                          offvalue = 0, width=20).place(x=500, y=200)
+c42 = tkinter.Checkbutton(quiz_anime, text = "isekai", indicatoron=0,
+                          variable = genero_anime2, onvalue = "isekai",
+                          offvalue = 0, width=20).place(x=500, y=220)
+c43 = tkinter.Checkbutton(quiz_anime, text = "supepoderes", indicatoron=0,
+                          variable = genero_anime3, onvalue = "supepoderes",
+                          offvalue = 0, width=20).place(x=500, y=240)
+c44 = tkinter.Checkbutton(quiz_anime, text = "mecha", indicatoron=0,
+                          variable = genero_anime4, onvalue = "mecha",
+                          offvalue = 0, width=20).place(x=500, y=260)
+c45 = tkinter.Checkbutton(quiz_anime, text = "bl", indicatoron=0,
+                          variable = genero_anime5, onvalue = "bl",
+                          offvalue = 0, width=20).place(x=500, y=280)
+c46 = tkinter.Checkbutton(quiz_anime, text = "gl", indicatoron=0,
+                          variable = genero_anime6, onvalue = "gl",
+                          offvalue = 0, width=20).place(x=500, y=300)
+c47 = tkinter.Checkbutton(quiz_anime, text = "shoujo", indicatoron=0,
+                          variable = genero_anime7, onvalue = "shoujo",
+                          offvalue = 0, width=20).place(x=500, y=320)
+c48 = tkinter.Checkbutton(quiz_anime, text = "escolar", indicatoron=0,
+                          variable = genero_anime8, onvalue = "escolar",
+                          offvalue = 0, width=20).place(x=500, y=340)
+c49 = tkinter.Checkbutton(quiz_anime, text = "slice of life", indicatoron=0,
+                          variable = genero_anime9, onvalue = "slice of life",
+                          offvalue = 0, width=20).place(x=500, y=360)
+c410 = tkinter.Checkbutton(quiz_anime, text = "josei", indicatoron=0,
+                          variable = genero_anime10, onvalue = "josei",
+                          offvalue = 0, width=20).place(x=500, y=380)
+c411 = tkinter.Checkbutton(quiz_anime, text = "seinen", indicatoron=0,
+                          variable = genero_anime11, onvalue = "seinen",
+                          offvalue = 0, width=20).place(x=500, y=400)
+c412 = tkinter.Checkbutton(quiz_anime, text = "gore", indicatoron=0,
+                          variable = genero_anime12, onvalue = "gore",
+                          offvalue = 0, width=20).place(x=500, y=420)
 """
 Pregunta de si les gustan los deportes o no (radiobutton)
 al elegir la opcion de si, se despeglaran las preguntas (solo si es serie)
 Teniendo varias opciones para escoger, siendo un maxico de 2 las que
 el usuario podrá elegir (checkbutton)
 """
-print("¿Te gustan los deportes? Si sí, escoge cuales (maximo 2)")
-def deporte (radiobutton5):
-    if radiobutton5 == si:
-        if checkbutton5 == football:
-            res = 1
-        elif checkbutton5 == baseball:
-            res = 2
-        elif checkbutton5 == basquetball:
-            res = 3
-        elif checkbutton5 == natacion:
-            res = 4
-        elif checkbutton5 == golf:
-            res = 5
-        elif checkbutton5 == tenis:
-            res = 6
-        elif checkbutton5 == gymnasia:
-            res = 7
-        elif checkbutton5 == patinaje:
-            res = 8
-        elif checkbutton5 == skate:
-            res = 9
-    elif radiobutton5 == no:
-        res = 1
-    else:
-        res = 0
-
+text6 = tkinter.Label(quiz_anime,
+                      text="¿Te gustan los deportes? Si sí,escoge cuales (maximo 2)")
+text6.pack(anchor="w")
+eleccion5 = ["Si","No"]
+saberdep = tkinter.StringVar()
+for index in range(len(eleccion5)):
+    radiobutton5 = tkinter.Radiobutton(quiz_anime, text= eleccion5[index],
+                                       variable = saberdep,indicatoron=0,
+                                       value=index, width=10).pack(anchor="w")
 """
 Una vez esten hechas las funciones se podran compilar las opciones
 y teniendo animes que tengan un rango dependiendo de cada pregunta
